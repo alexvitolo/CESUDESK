0a65c7b0b5b58d2233b012f89c080a73a1b69718
ADM-CESUMAR\alexandre.vitolo <alexandre.vitolo@unicesumar.edu.br>
2017-09-04 18:01:25 -0300

    Correções Campo OBS

--- AdmCrm/cadastroColaborador.php
+++ AdmCrm/cadastroColaborador.php
@@ -391,7 +391,7 @@ sqlsrv_execute($result_Grupo);
       <footer class="site-footer">
           <div class="text-center">
               2017 - ANALYTICS EAD
-              <a href="basic_table.html#" class="go-top">
+              <a href="" class="go-top">
                   <i class="fa fa-angle-up"></i>
               </a>
           </div>
--- AdmCrm/processo.php
+++ AdmCrm/processo.php
@@ -218,7 +218,7 @@ sqlsrv_execute($result_squilaProcesso);
       <footer class="site-footer">
           <div class="text-center">
               2017 - ANALYTICS EAD
-              <a href="basic_table.html#" class="go-top">
+              <a href="" class="go-top">
                   <i class="fa fa-angle-up"></i>
               </a>
           </div>
