df833ad86e3320fbcf0b8cda6ec3dc9aa8c9a4ff
ADM-CESUMAR\alexandre.vitolo <alexandre.vitolo@unicesumar.edu.br>
2017-10-05 09:00:06 -0300

    Scroll Bar General.css

--- AdmCrm/dimensionamento.php
+++ AdmCrm/dimensionamento.php
@@ -26,6 +26,7 @@ if ($_SESSION['ACESSO'] <> 1 )  {
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <link rel="shortcut icon" href="icone.ico" >
+     <link rel="stylesheet" href="..\AdmCrm\general.css">
     <!--external css-->
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
         
