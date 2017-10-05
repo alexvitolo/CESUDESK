52c82fa8b88f369270705cf2be08ff3a3ae2cfe1
ADM-CESUMAR\alexandre.vitolo <alexandre.vitolo@unicesumar.edu.br>
2017-10-05 16:26:32 -0300

    Melhoria Teste Conhecimento

--- AdmCrm/ValidaAvaliacaoConhecimento.php
+++ AdmCrm/ValidaAvaliacaoConhecimento.php
@@ -7,53 +7,102 @@ if ( ! isset( $_SESSION['USUARIO'] ) && ! isset( $_SESSION['ACESSO'] ) ) {
 echo  '<script type="text/javascript"> window.location.href = "http://d42150:8080/login"  </script>'; }
 
 
+ $ID_CONHECIMENTO =  $_POST['ID_CONHECIMENTO'];
+ $ID_COLABORADOR_CONSULTOR =  $_POST['ID_COLABORADOR_CONSULTOR'];
+
+ $NotaFinal = 0;
+$QUEM_REALIZOU = $_SESSION["USUARIO"];
+
+
+$insertTestePai = "INSERT INTO tb_ava_teste_conhecimento
+                                       (ID_CONHECIMENTO
+                                       ,ID_COLABORADOR
+                                       ,QUEM_REALIZOU
+                                         )
+                                 VALUES
+                                       ({$ID_CONHECIMENTO}
+                                       ,{$ID_COLABORADOR_CONSULTOR}
+                                       ,'{$QUEM_REALIZOU}'
+                                        ) ";
+
+ $result_insertTestePAi = sqlsrv_query($conn, $insertTestePai);
+
+      if (!($result_insertTestePAi)) {
+             // echo ("Falha na inclusão do registro");
+             print_r(sqlsrv_errors());
+             sqlsrv_close($conn);
+             echo  '<script type="text/javascript"> alert("Falha na inclusão do registro"); window.location.href = "testeconhecimento.php" </script>';
+      }   
+      else {
+            sqlsrv_free_stmt($result_insertTestePAi);
+        }
+
+
+
+//INSERT TABELA PESQUISA
+
+foreach( $_POST['vetorquestaorepostas'] as $key => $value ){ 
+      $QUESTAO = $key; // id_questao
+      $REPOSTA = $value; // id_resp
+
+
+       $sqlCalculaNota ="SELECT tq.ID_RESPOSTA
+                                   ,tq.BO_VERDADEIRO
+                           FROM tb_ava_questoes_conhecimento_alt tq
+                          WHERE tq.ID_RESPOSTA ={$REPOSTA}
+                         ";
+
+          $stmtNota = sqlsrv_prepare($conn, $sqlCalculaNota);
+          sqlsrv_execute($stmtNota);
+          $resultadoNota = sqlsrv_fetch_array($stmtNota);
+
+          if ($resultadoNota['BO_VERDADEIRO'] == 'S'){
+          	$NotaFinal = $NotaFinal + 1  ;
+          }
+
+$insertSquilaRespostas = " INSERT INTO tb_ava_teste_conhecimento_resp
+                                       (ID_TESTE
+                                       ,ID_QUESTAO
+                                       ,ID_RESPOSTA
+                                       )
+                                 VALUES
+                                       ((SELECT TOP 1 ID_TESTE FROM tb_ava_teste_conhecimento ORDER BY 1 DESC)
+                                       ,{$QUESTAO}
+                                       ,{$REPOSTA}
+                                      )";
+                            
+                   
+ $result_insertRespostas = sqlsrv_query($conn, $insertSquilaRespostas);
+
+      if (!($result_insertRespostas)) {
+             // echo ("Falha na inclusão do registro");
+             print_r(sqlsrv_errors());
+             sqlsrv_close($conn);
+             echo  '<script type="text/javascript"> alert("Falha na inclusão do registro"); window.location.href = "testeconhecimento.php" </script>';
+      }   
+      else {
+            sqlsrv_free_stmt($result_insertRespostas);
+        }
+
+
+} 
+
+
+$updateNotaFinal = " UPDATE tb_ava_teste_conhecimento
+                              SET 
+                            NOTA_FINAL = '{$NotaFinal}'
+                                 
+                          WHERE  ID_TESTE = (SELECT TOP 1 ttes.ID_TESTE FROM tb_ava_teste_conhecimento_resp ttes ORDER BY 1 DESC) ";
+
+  $result_updateNotaFinal = sqlsrv_query($conn, $updateNotaFinal);
+   sqlsrv_free_stmt($result_updateNotaFinal);
+
+
+
+ echo  '<script type="text/javascript">alert("Formulário Cadastrado");</script>';
+ echo  '<script type="text/javascript"> window.location.href = "testeconhecimento.php" </script>';
+
+
+
 
 ?>
-
-<html>
-<head>
-<style type="text/css">
-#wrapper {
-	
-	width:950px;
-	 height:auto;
-	 padding: 13px;
-	 margin-right:auto;
-	 margin-left:auto;
-	 background-color:#fff;
-}
-</style>
-</head>
-<?php 
-
-	$fid = $_GET['id'];
-
-?>
-<body bgcolor="#e1e1e1">
-
-<div id="wrapper">
-
-<center><font face="Andalus" size="5">Your Score</font></center>
-<br />
-<br />
-
-<?php
-	$answer1= $_POST['answerOne'];
-	$answer2= $_POST['answerTwo'];
-	$answer3= $_POST['answerThree'];
-	$score = 0;
-	
-	if ($answer1 == "A"){$score++;}
-	if ($answer2 == "B"){$score++;}
-	if ($answer3 == "C"){$score++;}
-	echo "<center><font face='Berlin Sans FB' size='8'>Your Score is <br> $score/3</font></center>";
-	
-?>
-
-
-
-
-</div><!-- end of wrapper div -->
-
-</body>
-</html>
