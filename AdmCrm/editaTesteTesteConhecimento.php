a427546d3c8b2a84f1cd1af90b0ef8303c321ab5
ADM-CESUMAR\alexandre.vitolo <alexandre.vitolo@unicesumar.edu.br>
2018-01-02 16:33:24 -0200

    Update Icones

--- AdmCrm/editaTesteTesteConhecimento.php
+++ AdmCrm/editaTesteTesteConhecimento.php
@@ -84,7 +84,8 @@ sqlsrv_execute($result_Questoes);
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->  <!-- pagina  off bloqueada -->
+<link rel="stylesheet" href="..\PlanilhaTrocas\font-awesome\css\font-awesome.css">
 <style>
 html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
 </style>
