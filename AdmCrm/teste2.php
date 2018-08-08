49adc5a352638f4a26c788675f383ccbdeb345bb
ADM-CESUMAR\alexandre.vitolo <alexandre.vitolo@unicesumar.edu.br>
2018-08-08 14:56:47 -0300

    no message

---
+++ AdmCrm/teste2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+                            <HTML>
+                            <HEAD><TITLE></TITLE> <!-- Verificar titulo -->
+                            <META content="text/html; charset=utf-8" http-equiv=Content-Type>
+                            <META name=GENERATOR content="MSHTML 8.00.7601.18094"></HEAD>
+                            <BODY style="PADDING-BOTTOM: 0px; MARGIN: 0px; PADDING-LEFT: 0px; PADDING-RIGHT: 0px; FONT-FAMILY: HELVETICA NEUE, HELVETICA; FONT-SIZE: 10pt; PADDING-TOP: 0px" bgColor=#ffffff>
+                                <TABLE style="FONT-FAMILY: HELVETICA NEUE, HELVETICA; COLOR: #6c6c6c" border=0 cellSpacing=0 cellPadding=0 width="100%">
+                                    <TBODY>
+                                        <TR>
+                                            <TD>
+                                                <TABLE style="BACKGROUND-COLOR: #fff;  BORDER-COLLAPSE: collapse; " border=0 cellSpacing=0 cellPadding=0 width=600 align=center>
+                                                <TBODY>
+                                                    <TR>
+                                                    <TD align=middle width=600 > 
+                            
+                                                            <IMG  style="display:block;" width=600 height=58 alt="EAD Unicesumar - Estude do seu jeito" src="http://imagens.ead.cesumar.br/crm/imgs/img-topo-2.jpg"  >
+                            
+                                                    </TD>
+                                                    </TR>
+                                                </TBODY>
+                                                <TBODY>
+                                                    <TR>
+                                                    <TD style=" PADDING-LEFT: 0px; PADDING-RIGHT: 0px; PADDING-TOP: 0px; FONT-SIZE: 24PX; " >
+                                                        <IMG style="display:block;" WIDTH = 600px;  alt="EAD Unicesumar - Estude do seu jeito" src="http://imagens.ead.cesumar.br/crm/imgs/topo-tabela-cesudesk.png" >              
+                                                    </TD>
+                                                    </TR>                   
+                                                </TBODY>
+                                                </TABLE><br><br>
+                                                <TABLE style="BACKGROUND-COLOR: #fff; border-color: gray;  BORDER-COLLAPSE: collapse; " border=1 cellSpacing=0 cellPadding=0 width=600 align=center>
+                                                <TBODY>
+                                                    <TR>
+                                                    <TD align= middle style="background-color: #003d65; color: white; border: none; "> 
+                                                        <span><b>Matr&iacute;cula</b></span>
+                                                    </TD>
+                                                    <TD align= middle style="background-color: #003d65; color: white; border: none;"> 
+                                                        <span><b>Consultor</b></span>
+                                                    </TD>
+                                                    <TD align= middle style="background-color: #003d65; color: white; border: none;"> 
+                                                        <span><b>Senha</b></span>
+                                                    </TD>
+                                                    <TD align= middle style="background-color: #003d65; color: white; border: none;"> 
+                                                        <span><b>Link Senha</b></span>
+                                                    </TD>
+                                                    </TR><TR><TD align= middle>17271</TD><TD align= middle>Alexandre Henrique Vieira Vitolo</TD><TD align= middle></TD><TD align= middle> <a href="dd42150:8087/CESUDESK/Altera_Senha_FeedBack.php?ID_MATRICULA=MTcyNzE=" >CLIQUE AQUI </a></TD></TR></TBODY>
+                          </TABLE>                  
+                          </BODY>
+                          </HTML>
